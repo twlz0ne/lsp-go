d270b7c0bd893fdc73a97763e07706830756cad2
Vibhav Pant <vibhavp@gmail.com>
2017-10-21 16:05:25 +0530

    Use new API

--- lsp-go.el
+++ lsp-go.el
@@ -4,18 +4,17 @@
 
 ;; Author: Vibhav Pant <vibhavp@gmail.com>
 ;; Version: 1.0
-;; Package-Requires: ((lsp-mode "2.0") (go-mode "1.0.0"))
+;; Package-Requires: ((lsp-mode "3.0"))
 ;; Keywords: go, golang
 ;; URL: https://github.com/emacs-lsp/lsp-go
 
 (require 'lsp-mode)
-(require 'go-mode)
 
 ;;;###autoload
-(lsp-define-stdio-client 'go-mode "go" 'stdio #'(lambda () default-directory)
-			 "Go Language Server"
+(lsp-define-stdio-client lsp-go "go" #'(lambda () default-directory)
 			 '("go-langserver" "-mode=stdio")
-			 :ignore-regexps '("^langserver-go: reading on stdin, writing on stdout$"))
+			 :ignore-regexps
+			 '("^langserver-go: reading on stdin, writing on stdout$"))
 
 (provide 'lsp-go)
 ;;; lsp-go.el ends here
